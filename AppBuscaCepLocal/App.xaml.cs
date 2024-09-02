3c0afa2d96a28a9173a538440bd0d1be6f694601
Usuario <Usuario@CPD03-M01>
2024-09-02 12:01:38 -0300

    Adicionar arquivos de projeto.

---
+++ AppBuscaCepLocal/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppBuscaCepLocal
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
